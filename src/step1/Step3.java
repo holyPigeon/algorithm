a407015214d623c6357f88d06fb47b0e71efaad5
jae woo Jung <awfjol2008@naver.com>
2022-12-08 22:05:59 +0900

    Step3

--- src/step1/Step3.java
+++ src/step1/Step3.java
@@ -2,13 +2,13 @@ package step1;
 
 import java.util.Scanner;
 
-public class Step2 {
+public class Step3 {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
 
     int x1 = scanner.nextInt();
     int x2 = scanner.nextInt();
 
-    System.out.println(x1 + x2);
+    System.out.println(x1 - x2);
   }
 }
