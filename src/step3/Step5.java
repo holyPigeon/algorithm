9401278a1968d50baed331d726d008d0f8e1cd41
jae woo Jung <awfjol2008@naver.com>
2022-12-11 00:19:41 +0900

    3-5

---
+++ src/step3/Step5.java
@@ -0,0 +1,34 @@
+package step3;
+
+import java.io.*;
+
+public class Step5 {
+  public static void main(String[] args) throws IOException {
+    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    String countStr = reader.readLine();
+    int count = Integer.parseInt(countStr);
+
+    int[] number = new int[count * 2];
+    int[] sum = new int[count];
+
+    for (int i = 0; i < count; i++) {
+      String[] str = reader.readLine().split(" ");
+      number[2 * i] = Integer.parseInt(str[0]);
+      number[2 * i + 1] = Integer.parseInt(str[1]);
+      sum[i] = number[2 * i] + number[2 * i + 1];
+    }
+
+    for (int j = 0; j < count; j++) {
+      if (j == (count - 1)) {
+        bw.write(Integer.toString(sum[j]));
+      } else {
+        bw.write(sum[j] + "\n");
+      }
+
+    }
+
+    bw.flush();
+  }
+}
