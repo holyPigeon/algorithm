36f403e31e79d1ebcaaeaa2a98c416ec35580267
jae woo Jung <awfjol2008@naver.com>
2022-12-18 19:24:55 +0900

    5-1

---
+++ src/step5/Step1.java
@@ -0,0 +1,19 @@
+package step5;
+
+import java.io.*;
+
+public class Step1 {
+  public static void main(String[] args) throws IOException {
+
+  }
+
+//  long sum(int[] a) {
+//    int sum = 0;
+//    for (int i = 0; i < a.length; i++) {
+//      sum += a[i];
+//    }
+//
+//    return sum;
+//  }
+}
+
