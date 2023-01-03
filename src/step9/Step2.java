e28af10eca81ac2e9180bbe4136d7c0b41503143
jae woo Jung <awfjol2008@naver.com>
2023-01-03 19:08:53 +0900

    9-2

---
+++ src/step9/Step2.java
@@ -0,0 +1,39 @@
+package step9;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Step2 {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    int[][] arr = new int[9][9];
+    int max, row = 0, col = 0;
+
+    for (int i = 0; i < 9; i++) {
+      String str = br.readLine();
+      StringTokenizer st = new StringTokenizer(str, " ");
+      for (int j = 0; j < 9; j++) {
+        arr[i][j] = Integer.parseInt(st.nextToken());
+      }
+    }
+
+    max = arr[0][0];
+
+    for (int i = 0; i < 9; i++) {
+      for (int j = 0; j < 9; j++) {
+        if (arr[i][j] >= max) {
+          max = arr[i][j];
+          row = i + 1;
+          col = j + 1;
+        }
+      }
+    }
+
+    bw.write(max + "\n");
+    bw.write(row + " " + col);
+
+    bw.flush();
+  }
+}
