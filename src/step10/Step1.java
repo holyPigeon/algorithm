9db251b2bc072dad23903258898a5f78bf9dcca6
jae woo Jung <awfjol2008@naver.com>
2023-01-06 00:00:10 +0900

    10-1

---
+++ src/step10/Step1.java
@@ -0,0 +1,14 @@
+package step10;
+
+import java.io.*;
+
+public class Step1 {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    String str1 = br.readLine();
+
+    bw.flush();
+  }
+}
