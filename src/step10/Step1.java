20db746b91ed009cf32dc72252ad169e54f6f338
jae woo Jung <awfjol2008@naver.com>
2023-01-06 18:33:34 +0900

    10-1 2

--- src/step10/Step1.java
+++ src/step10/Step1.java
@@ -1,6 +1,9 @@
 package step10;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Step1 {
   public static void main(String[] args) throws IOException {
@@ -8,6 +11,19 @@ public class Step1 {
     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
     String str1 = br.readLine();
+    int testcase = Integer.parseInt(str1);
+    List<Integer> list = new ArrayList<>();
+
+    for (int i = 0; i < testcase; i++) {
+      String str2 = br.readLine();
+      list.add(Integer.parseInt(str2));
+    }
+
+    Collections.sort(list);
+
+    for (int i: list) {
+      bw.write(i + "\n");
+    }
 
     bw.flush();
   }
