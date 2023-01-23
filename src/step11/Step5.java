32bbd0c1ea6c3f12ea56faf3b3342a1a590aea9e
jae woo Jung <awfjol2008@naver.com>
2023-01-23 16:53:15 +0900

    11-5

---
+++ src/step11/Step5.java
@@ -0,0 +1,22 @@
+package step11;
+
+import java.io.*;
+
+public class Step5 {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    int n = Integer.parseInt(br.readLine());
+    int count = 0;
+
+    while (n >= 3) {
+      n /= 3;
+      count++;
+    }
+
+    bw.write(count + " ");
+
+    bw.flush();
+  }
+}
