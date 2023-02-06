93261f770e9bdb5574f6e52b332a48ea6c172d80
jae woo Jung <awfjol2008@naver.com>
2023-02-07 00:00:07 +0900

    SelectionSort

--- src/algorithm/sortAlgorithm/BubbleSort.java
+++ src/algorithm/sortAlgorithm/BubbleSort.java
@@ -1,36 +1,20 @@
 package algorithm.sortAlgorithm;
 
 // 1 10 5 8 7 6 4 3 2 9
-// 인덱스 i부터 시작해서 i ~ 마지막 까지 순회하다가 자신보다 작은 숫자가 나오면 바꾸고 종료. 이후 인덱스는 +1 된다.
+// 인덱스 i부터 시작해서 i ~ length - i 까지 순회하면서
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class SelectionSort {
+public class BubbleSort {
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     StringTokenizer st = new StringTokenizer(br.readLine(), " ");
     int length = st.countTokens();
     int[] num = new int[length];
     int min, temp, index;
-
-    for (int i = 0; i < length; i++) {
-      num[i] = Integer.parseInt(st.nextToken());
-    }
-
-//    min = num[0];
-
-      for (int i = 0; i < length; i++) {
-        for (int j = i; j < length; j++) {
-          if (num[j] < num[i]) {
-            temp = num[i];
-            num[i] = num[j];
-            num[j] = temp;
-          }
-        }
-      }
 
     for (int i : num) {
       System.out.println(i + " ");
