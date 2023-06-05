64c56bb1604df60c6393a6fbc60cef8d43981952
jae woo Jung <awfjol2008@naver.com>
2023-06-06 00:50:18 +0900

    문제 풀이(미완)

---
+++ src/baekjoon/math/problem_2903.java
@@ -0,0 +1,12 @@
+package baekjoon.math;
+
+public class problem_2903 {
+  public static void main(String[] args) {
+
+    int initialState = 4;
+    int surroundSuare = 0;
+    int existingPoint = 4;
+    int newPoint;
+
+  }
+}
