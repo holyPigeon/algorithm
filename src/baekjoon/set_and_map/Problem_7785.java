2a8c3bf93ea364dc0ccae28845a29f663e4bcb0f
jae woo Jung <awfjol2008@naver.com>
2023-07-07 22:12:50 +0900

    7785 문제 풀이 설명 변경

--- src/baekjoon/set_and_map/Problem_7785.java
+++ src/baekjoon/set_and_map/Problem_7785.java
@@ -2,13 +2,12 @@ package baekjoon.set_and_map;
 
 import java.io.*;
 import java.util.Comparator;
-import java.util.HashSet;
 import java.util.StringTokenizer;
 import java.util.TreeSet;
-import java.util.stream.Collectors;
 
 /*
   - 입력에 따라 집합에서 문자열을 추가, 또는 제거하여 마지막에 집합에 남은 문자열을 출력하기
+  -> 정렬된 집합을 사용해야 하므로 HashSet 대신 TreeSet 사용
 
   1. 첫째줄에서 n을 입력으로 받아들인다.
   2. n만큼 반복문을 돌려서 StringTokenizer의 두 번째 토큰이 enter인 경우 첫 번째 토큰 문자열을 집합에 추가하고, leave라면 삭제한다.
