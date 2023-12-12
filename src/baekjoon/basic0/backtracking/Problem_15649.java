a572aa144ecb716fea40e14ca3f005d5564b778e
jae woo Jung <awfjol2008@naver.com>
2023-12-12 13:55:55 +0900

    15649 문제 풀이

---
+++ src/baekjoon/basic0/backtracking/Problem_15649.java
@@ -0,0 +1,19 @@
+package baekjoon.basic0.backtracking;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Problem_15649 {
+  public static void main(String[] args) throws IOException {
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    StringBuilder sb = new StringBuilder();
+
+    StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+    int n = Integer.parseInt(st.nextToken());
+    int m = Integer.parseInt(st.nextToken());
+
+    
+  }
+}
