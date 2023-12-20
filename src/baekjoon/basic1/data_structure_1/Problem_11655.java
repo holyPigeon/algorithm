0942f3adf9c9638828f6ad6534b503575ba7b872
jae woo Jung <awfjol2008@naver.com>
2023-12-20 21:04:00 +0900

    11655 문제 풀이 완료

--- src/baekjoon/basic1/data_structure_1/Problem_11655.java
+++ src/baekjoon/basic1/data_structure_1/Problem_11655.java
@@ -1,14 +1,28 @@
 package baekjoon.basic1.data_structure_1;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
 
 public class Problem_11655 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
+
+        String input = br.readLine();
+
+        for (int i = 0; i < input.length(); i++) {
+            char ch = input.charAt(i);
+            if (Character.isLowerCase(ch)) {
+                sb.append((char)((ch - 'a' + 13) % 26 + 'a'));
+            } else if (Character.isUpperCase(ch)) {
+                sb.append((char) ((ch - 'A' + 13) % 26 + 'A'));
+            } else {
+                sb.append(ch);
+            }
+        }
+
+        bw.write(sb.toString());
+        bw.close();
+        br.close();
     }
 }
