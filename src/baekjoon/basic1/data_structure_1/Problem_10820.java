06845432a65bee2e5f69ce583e0eb4ab4dd7deea
jae woo Jung <awfjol2008@naver.com>
2023-12-20 20:29:44 +0900

    10820 문제 풀이 시작

---
+++ src/baekjoon/basic1/data_structure_1/Problem_10820.java
@@ -0,0 +1,12 @@
+package baekjoon.basic1.data_structure_1;
+
+import java.io.*;
+
+public class Problem_10820 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+    }
+}
