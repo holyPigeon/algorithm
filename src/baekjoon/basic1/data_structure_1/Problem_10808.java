4e1913898501159010f979608507ed86b1ff980a
jae woo Jung <awfjol2008@naver.com>
2023-12-20 20:28:18 +0900

    10808 문제 풀이 완료

--- src/baekjoon/basic1/data_structure_1/Problem_10808.java
+++ src/baekjoon/basic1/data_structure_1/Problem_10808.java
@@ -1,14 +1,27 @@
 package baekjoon.basic1.data_structure_1;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
+import java.util.ArrayList;
 
 public class Problem_10808 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
+
+        String input = br.readLine();
+        ArrayList<Integer> list = new ArrayList<>();
+
+
+        for (int i = 0; i < 26; i++) {
+            list.add(0);
+            for (int j = 0; j < input.length(); j++) {
+                if ((int)input.charAt(j) == i + 97) {
+                    list.set(i, list.get(i) + 1);
+                }
+            }
+        }
+
+        list.forEach(number -> System.out.print(number + " "));
     }
 }
