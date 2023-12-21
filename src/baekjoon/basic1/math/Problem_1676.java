00f0fc3047489613e4a5d394ee107ec7d99ed796
jae woo Jung <awfjol2008@naver.com>
2023-12-21 18:16:08 +0900

    11655 문제 풀이 완료

---
+++ src/baekjoon/basic1/math/Problem_1676.java
@@ -0,0 +1,19 @@
+package baekjoon.basic1.math;
+
+import java.util.Scanner;
+
+public class Problem_1676 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int num = in.nextInt();
+        int count = 0;
+
+        while (num >= 5) {
+            count += num / 5;
+            num /= 5;
+        }
+        System.out.println(count);
+    }
+}
