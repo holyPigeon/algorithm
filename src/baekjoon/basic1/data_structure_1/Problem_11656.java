1e1fa9a68731fd5b7092ebc8141658822c50d840
jae woo Jung <awfjol2008@naver.com>
2023-12-21 18:16:00 +0900

    11655 문제 풀이 완료

--- src/baekjoon/basic1/data_structure_1/Problem_11656.java
+++ src/baekjoon/basic1/data_structure_1/Problem_11656.java
@@ -1,14 +1,24 @@
 package baekjoon.basic1.data_structure_1;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Problem_11656 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
+
+        List<Object> list = new ArrayList<>();
+        String input = br.readLine();
+
+        for (int i = 0; i < input.length(); i++) {
+            list.add(input.substring(i, input.length()));
+        }
+
+        list.stream()
+                .sorted()
+                .forEach(System.out::println);
     }
 }
