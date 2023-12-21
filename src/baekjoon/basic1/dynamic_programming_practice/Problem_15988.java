b60049f30f222d85df66f1d207e7da4bcf7c0173
jae woo Jung <awfjol2008@naver.com>
2023-12-21 18:46:03 +0900

    15988 문제 풀이 시작

---
+++ src/baekjoon/basic1/dynamic_programming_practice/Problem_15988.java
@@ -0,0 +1,16 @@
+package baekjoon.basic1.dynamic_programming_practice;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Problem_15988 {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+
+    }
+}
