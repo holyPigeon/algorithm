c0ae32bf1b8b482482f0408fd75b38525046ccbd
jae woo Jung <awfjol2008@naver.com>
2023-12-21 16:56:59 +0900

    11655 문제 풀이 시작

---
+++ src/baekjoon/basic1/data_structure_1/Problem_11656.java
@@ -0,0 +1,14 @@
+package baekjoon.basic1.data_structure_1;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Problem_11656 {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+    }
+}
