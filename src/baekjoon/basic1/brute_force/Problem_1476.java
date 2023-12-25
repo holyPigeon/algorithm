753f009e6888654018cc8fd056bbb3ec5bcb7ec5
jae woo Jung <awfjol2008@naver.com>
2023-12-25 18:55:44 +0900

    1476 문제 풀이 시작

---
+++ src/baekjoon/basic1/brute_force/Problem_1476.java
@@ -0,0 +1,11 @@
+package baekjoon.basic1.brute_force;
+
+import java.io.*;
+
+public class Problem_1476 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+    }
+}
