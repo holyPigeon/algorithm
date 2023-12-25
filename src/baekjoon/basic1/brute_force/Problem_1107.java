1205fcda6b54aed873a07e9ccf3a6dcb6abf6429
jae woo Jung <awfjol2008@naver.com>
2023-12-26 08:33:04 +0900

    1107 문제 풀이 시작

---
+++ src/baekjoon/basic1/brute_force/Problem_1107.java
@@ -0,0 +1,23 @@
+package baekjoon.basic1.brute_force;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+theme: 리모컨으로 채널 이동을 하려는데 리모컨이 고장났다.
+
+- 고장난 숫자 버튼을 토대로 해당 버튼을 사용하지 않고 몇 번 버튼을 눌러야 N에 도달할 수 있는지 알아낸다.
+ */
+
+public class Problem_1107 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int initialChannel = 100;
+        int count = 0;
+        int n;
+    }
+}
