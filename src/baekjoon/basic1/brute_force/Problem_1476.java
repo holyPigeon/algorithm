6cebac9388cadda6856b348e404642702059c5cb
jae woo Jung <awfjol2008@naver.com>
2023-12-25 18:55:51 +0900

    1476 문제 풀이 완료

--- src/baekjoon/basic1/brute_force/Problem_1476.java
+++ src/baekjoon/basic1/brute_force/Problem_1476.java
@@ -7,5 +7,36 @@ public class Problem_1476 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
+
+        int e, s, m;
+        int x = 0, y = 0, z = 0;
+        int year = 0;
+
+        String input = br.readLine();
+        String[] numbers = input.split(" ");
+        e = Integer.parseInt(numbers[0]);
+        s = Integer.parseInt(numbers[1]);
+        m = Integer.parseInt(numbers[2]);
+
+        while (true) {
+            x++;
+            if (x == 16) {
+                x = 1;
+            }
+            y++;
+            if (y == 29) {
+                y = 1;
+            }
+            z++;
+            if (z == 20) {
+                z = 1;
+            }
+            year++;
+            if (x == e && y == s && z == m) {
+                break;
+            }
+        }
+
+        System.out.println(year);
     }
 }
