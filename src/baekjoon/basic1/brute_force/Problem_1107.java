1dfcfffcf728d4f3ef4f2de1a2c72d57a19dec7c
jae woo Jung <awfjol2008@naver.com>
2023-12-26 08:33:14 +0900

    1107 문제 풀이 진행

--- src/baekjoon/basic1/brute_force/Problem_1107.java
+++ src/baekjoon/basic1/brute_force/Problem_1107.java
@@ -19,5 +19,53 @@ public class Problem_1107 {
         int initialChannel = 100;
         int count = 0;
         int n;
+
+        n = Integer.parseInt(br.readLine()); // n 입력
+        List<Integer> availableButtons = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            availableButtons.add(i);
+        }
+
+        int brokenButtonCount = Integer.parseInt(br.readLine());
+        String[] brokenButtons = br.readLine().split(" ");
+
+        for (int i = 0; i < brokenButtonCount; i++) {
+            availableButtons.remove(Integer.valueOf(Integer.parseInt(brokenButtons[i])));
+        }
+
+        if (n != initialChannel) {
+            count = (n - findClosestNumber(availableButtons, n)) + String.valueOf(n).length();
+        }
+
+        System.out.println(count);
+        br.close();
+    }
+
+    public static int findClosestNumber(List<Integer> availableButtons, int targetNumber) {
+        int closestNumber = 0;
+        int minDifference = Integer.MAX_VALUE;
+
+        // 0부터 9까지 사용 가능한 숫자로 만들 수 있는 경우의 수 계산
+        for (int i = 0; i < (1 << availableButtons.size()); i++) {
+            int currentNumber = getNumberFromSubset(availableButtons, i);
+            int difference = Math.abs(targetNumber - currentNumber);
+
+            if (difference < minDifference) {
+                minDifference = difference;
+                closestNumber = currentNumber;
+            }
+        }
+
+        return closestNumber;
+    }
+
+    public static int getNumberFromSubset(List<Integer> availableDigits, int subset) {
+        int number = 0;
+        for (int i = 0; i < availableDigits.size(); i++) {
+            if ((subset & (1 << i)) != 0) {
+                number = number * 10 + availableDigits.get(i);
+            }
+        }
+        return number;
     }
 }
