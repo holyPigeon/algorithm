99a30768e8c2f93a876e48621fc50ddad9d35884
jae woo Jung <awfjol2008@naver.com>
2023-12-27 20:51:20 +0900

    9095 문제 풀이 시작

---
+++ src/baekjoon/basic1/brute_force/Problem_9095.java
@@ -0,0 +1,16 @@
+package baekjoon.basic1.brute_force;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Problem_9095 {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+
+    }
+}
