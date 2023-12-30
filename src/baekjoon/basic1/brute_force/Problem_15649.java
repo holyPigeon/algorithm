7068e64899240e085152ef1d6c9708069042e2e6
jae woo Jung <awfjol2008@naver.com>
2023-12-30 22:21:00 +0900

    15649 문제 풀이 중단
    DFS를 익혀야 풀이가 수월한 문제라 생각되어 DFS 개념을 먼저 선행학습한 후 진행하기로 결정

--- src/baekjoon/basic1/brute_force/Problem_15649.java
+++ src/baekjoon/basic1/brute_force/Problem_15649.java
@@ -1,14 +1,30 @@
 package baekjoon.basic1.brute_force;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
+
+/*
+theme: 주어진 N, M에 따라 수열을 출력한다.
+
+- 사실상 nPm 의 개별적인 경우의 수를 나열한 것과 같다.
+-
+ */
 
 public class Problem_15649 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
+
+        String[] tokens = br.readLine().split(" ");
+        int n = Integer.parseInt(tokens[0]);
+        int m = Integer.parseInt(tokens[0]);
+
+        for (int i = 1; i <= n; i++) {
+
+        }
+    }
+
+    public static String findAvailableCases(int currentIndex, int n, int m) {
+
     }
 }
