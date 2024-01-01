9d59db738b945f19f6df1e41d105b96e40726d02
jae woo Jung <awfjol2008@naver.com>
2024-01-01 19:25:19 +0900

    1260 문제 풀이 진행
    dfs 구현

--- src/baekjoon/basic1/bfs/Problem_1260.java
+++ src/baekjoon/basic1/bfs/Problem_1260.java
@@ -7,7 +7,7 @@ theme: 탐색 결과를 각각 dfs, bfs 로 나타내자
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.List;
+import java.util.*;
 
 public class Problem_1260 {
 
@@ -18,6 +18,45 @@ public class Problem_1260 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        startV = Integer.parseInt(st.nextToken());
 
+        isVisited = new boolean[n + 1];
+        graph = new ArrayList[n + 1];
+
+        // init
+        for (int i = 0; i <= n; i++) {
+            graph[i] = new ArrayList<>();
+        }
+        for (int i = 1; i <= n; i++) {
+            isVisited[i] = false;
+        }
+        for (int i = 0; i < m; i++) {
+            StringTokenizer st1 = new StringTokenizer(br.readLine(), " ");
+            int a = Integer.parseInt(st1.nextToken());
+            int b = Integer.parseInt(st1.nextToken());
+            graph[a].add(b);
+            graph[b].add(a);
+        }
+        Arrays.stream(graph)
+                .forEach(Collections::sort);
+
+        // dfs logic
+        for (int i = 0; i < n; i++) {
+            dfs(startV);
+        }
+    }
+
+    public static void dfs(int v) {
+        if (isVisited[v]) {
+            return;
+        }
+        isVisited[v] = true;
+        System.out.print(v + " ");
+        for (Integer adjacentV : graph[v]) {
+            dfs(adjacentV);
+        }
     }
 }
