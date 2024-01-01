3e7424ea0d9dd18682641c0cfa7f178d65513b2c
jae woo Jung <awfjol2008@naver.com>
2024-01-01 19:24:41 +0900

    1260 문제 풀이 시작

---
+++ src/baekjoon/basic1/bfs/Problem_1260.java
@@ -0,0 +1,23 @@
+package baekjoon.basic1.bfs;
+
+/*
+theme: 탐색 결과를 각각 dfs, bfs 로 나타내자
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+
+public class Problem_1260 {
+
+    static int n, m, startV;
+    static List<Integer>[] graph;
+    static boolean[] isVisited;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+    }
+}
