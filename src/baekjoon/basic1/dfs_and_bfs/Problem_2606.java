52522d9648614e2e5ca86663fc4627b125e2c60b
jae woo Jung <awfjol2008@naver.com>
2024-01-02 09:32:41 +0900

    2606 문제 풀이 시작

---
+++ src/baekjoon/basic1/dfs_and_bfs/Problem_2606.java
@@ -0,0 +1,19 @@
+package baekjoon.basic1.dfs_and_bfs;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Problem_2606 {
+
+    static List<Integer>[] graph;
+    static boolean[] isVisited;
+    static int result = 0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    }
+}
