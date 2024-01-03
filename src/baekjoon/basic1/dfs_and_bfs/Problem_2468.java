dc0ac90572de51b91e920ab11aeab4548185ec50
jae woo Jung <awfjol2008@naver.com>
2024-01-03 22:53:16 +0900

    2468 문제 풀이 시작

---
+++ src/baekjoon/basic1/dfs_and_bfs/Problem_2468.java
@@ -0,0 +1,17 @@
+package baekjoon.basic1.dfs_and_bfs;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Problem_2468 {
+
+    static int n;
+    static int[][] map;
+    static boolean[][] isVisited;
+
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+    }
+}
