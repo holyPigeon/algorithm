c3ce5555a9e8e3e4fb7542491bb7e786c54f509f
jae woo Jung <awfjol2008@naver.com>
2024-01-07 16:56:05 +0900

    11047 문제 풀이 시작

---
+++ src/baekjoon/basic1/greedy/Problem_11047.java
@@ -0,0 +1,10 @@
+package baekjoon.basic1.greedy;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Problem_11047 {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    }
+}
