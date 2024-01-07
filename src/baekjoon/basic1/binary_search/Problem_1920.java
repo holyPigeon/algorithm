1428f01224899746f7ac0f14c6c76b044d95646e
jae woo Jung <awfjol2008@naver.com>
2024-01-07 16:41:22 +0900

    1920 문제 풀이 완료

--- src/baekjoon/basic1/binary_search/Problem_1920.java
+++ src/baekjoon/basic1/binary_search/Problem_1920.java
@@ -26,5 +26,35 @@ public class Problem_1920 {
         Arrays.sort(numbers);
 
         M = Integer.parseInt(br.readLine());
+
+        // logic
+        StringTokenizer st2 = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            int numberToCheck = Integer.parseInt(st2.nextToken());
+            if (binary_search(numbers, numberToCheck)) {
+                System.out.println(1);
+            } else {
+                System.out.println(0);
+            }
+        }
+    }
+
+    static private boolean binary_search(int[] numbers, int numberToCheck) {
+        int low = 0;
+        int high = numbers.length - 1;
+        int mid;
+
+        while (low <= high) {
+            mid = (high + low) / 2;
+            if (numberToCheck < numbers[mid]) {
+                high = mid - 1;
+            } else if (numberToCheck > numbers[mid]) {
+                low = mid + 1;
+            } else if (numberToCheck == numbers[mid]) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
