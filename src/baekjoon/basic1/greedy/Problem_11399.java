3d3f329c5db3c2c38d27642da781c4ae6625cb29
jae woo Jung <awfjol2008@naver.com>
2024-01-10 20:24:53 +0900

    11399 문제 풀이 완료

--- src/baekjoon/basic1/greedy/Problem_11399.java
+++ src/baekjoon/basic1/greedy/Problem_11399.java
@@ -1,4 +1,36 @@
 package baekjoon.basic1.greedy;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
 public class Problem_11399 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // init
+        int N = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] time = new int[N];
+        for (int i = 0; i < N; i++) {
+            time[i] = Integer.parseInt(st.nextToken());
+        }
+
+        // logic
+        Arrays.sort(time);
+
+        int result = 0;
+        for (int i = 0; i < N; i++) {
+            int sum = 0;
+            for (int j = i; j >= 0; j--) {
+                sum += time[j];
+            }
+            result += sum;
+        }
+
+        System.out.println(result);
+    }
 }
