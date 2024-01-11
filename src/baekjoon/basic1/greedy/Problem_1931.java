445db394479948092dc2931913992deac03ec473
jae woo Jung <awfjol2008@naver.com>
2024-01-11 23:01:40 +0900

    1931 문제 풀이 시작

---
+++ src/baekjoon/basic1/greedy/Problem_1931.java
@@ -0,0 +1,16 @@
+package baekjoon.basic1.greedy;
+
+import java.io.IOException;
+
+    /*
+    theme: 최대한 많이 때려박을 수 있는 회의의 개수를 알아보자
+
+    - 회의가 최대한 많이 들어가려면 회의 진행 시간 자체가 짧아야 한다.
+    - 겹치지 않게 들어가는 방법
+        - 앞 회의의 뒷 시간 <= 뒷 회의의 앞 시간
+     */
+
+public class Problem_1931 {
+    public static void main(String[] args) throws IOException {
+    }
+}
