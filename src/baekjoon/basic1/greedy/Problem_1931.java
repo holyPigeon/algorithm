ca19775203db301bf0e00741f9262cb74ce4cded
jae woo Jung <awfjol2008@naver.com>
2024-01-11 23:01:55 +0900

    1931 문제 풀이 완료

--- src/baekjoon/basic1/greedy/Problem_1931.java
+++ src/baekjoon/basic1/greedy/Problem_1931.java
@@ -1,6 +1,11 @@
 package baekjoon.basic1.greedy;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
 
     /*
     theme: 최대한 많이 때려박을 수 있는 회의의 개수를 알아보자
@@ -12,5 +17,30 @@ import java.io.IOException;
 
 public class Problem_1931 {
     public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[][] timeTable = new int[N][2];
+
+        // init
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            timeTable[i][0] = Integer.parseInt(st.nextToken());
+            timeTable[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(timeTable, Comparator.comparingInt((int[] array) -> array[1]).thenComparingInt(array -> array[0]));
+
+        // logic
+        int count = 1;
+        int latest = 0;
+        for (int i = 1; i < N; i++) {
+            if (timeTable[latest][1] <= (timeTable[i][0])) { // 회의 시간이 겹치지 않는지
+                latest = i;
+                count++;
+            }
+        }
+
+        System.out.println(count);
     }
 }
