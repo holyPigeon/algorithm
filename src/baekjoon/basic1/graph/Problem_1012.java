f7394de6783cd1a40661c4865fb069bbe2d35f63
jae woo Jung <awfjol2008@naver.com>
2024-01-14 17:13:31 +0900

    1012 문제 풀이 시작

---
+++ src/baekjoon/basic1/graph/Problem_1012.java
@@ -0,0 +1,10 @@
+package baekjoon.basic1.graph;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Problem_1012 {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    }
+}
