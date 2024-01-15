91c6021db8dad8e68b514c0a5276861d6c267493
jae woo Jung <awfjol2008@naver.com>
2024-01-15 20:40:14 +0900

    1976 문제 풀이 완료

--- src/baekjoon/basic1/union_find/Problem_1976.java
+++ src/baekjoon/basic1/union_find/Problem_1976.java
@@ -1,12 +1,77 @@
 package baekjoon.basic1.union_find;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Problem_1976 {
-    public static void main(String[] args) {
+
+    static int[] array;
+    static int[][] isConnected;
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+        array = new int[n + 1];
+        isConnected = new int[n][n];
 
+        // init
+        for (int i = 0; i < n + 1; i++) {
+            array[i] = i;
+        }
+
+        for (int i = 1; i < n + 1; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 1; j < n + 1; j++) {
+                int sign = Integer.parseInt(st.nextToken());
+
+                if (sign == 1) {
+                    union(i, j);
+                }
+            }
+        }
+
+        // logic
+        String[] st2 = br.readLine().split(" ");
+        boolean result = true;
+
+        for (int i = 0; i < m - 1; i++) {
+            if (!find(Integer.parseInt(st2[i]), Integer.parseInt(st2[i + 1]))) {
+                result =  false;
+            }
+        }
+
+        if (result) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+
+    }
+
+    private static void union(int x, int y) {
+        int a = findRoot(x);
+        int b = findRoot(y);
+        if (a != b) {
+            array[b] = a;
+        }
+    }
+
+    private static boolean find(int x, int y) {
+        if (findRoot(x) == findRoot(y)) {
+            return true;
+        }
+        return false;
+    }
+
+    private static int findRoot(int a) {
+        if (array[a] == a) {
+            return a;
+        } else {
+            return array[a] = findRoot(array[a]);
+        }
     }
 }
