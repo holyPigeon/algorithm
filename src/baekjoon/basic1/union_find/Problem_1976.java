9fc4cf31957e325abff076c8082f1ea4676f2b7b
jae woo Jung <awfjol2008@naver.com>
2024-01-15 19:05:40 +0900

    1976 문제 풀이 시작

---
+++ src/baekjoon/basic1/union_find/Problem_1976.java
@@ -0,0 +1,12 @@
+package baekjoon.basic1.union_find;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Problem_1976 {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+    }
+}
