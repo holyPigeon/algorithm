54adbb603b007f6b4050b44fa0c87ea062961d23
jae woo Jung <awfjol2008@naver.com>
2024-01-17 19:15:58 +0900

    1987 문제 풀이 시작

---
+++ src/baekjoon/basic1/graph/Problem_1987.java
@@ -0,0 +1,24 @@
+package baekjoon.basic1.graph;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Problem_1987 {
+
+    static int[] dx = new int[]{1, -1, 0, 0};
+    static int[] dy = new int[]{0, 0, 1, -1};
+    static int r, c;
+    static char[][] map;
+    static boolean[][] isVisited;
+    static Set<Character> visitedSquare;
+    static int count = 1;
+    static int max = 1;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    }
+}
