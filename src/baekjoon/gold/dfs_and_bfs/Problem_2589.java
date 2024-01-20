0efe874ccf751c7e4380a5a5099ba9ff93eeebf9
jae woo Jung <awfjol2008@naver.com>
2024-01-20 21:47:05 +0900

    2589 문제 풀이 진행

--- src/baekjoon/gold/dfs_and_bfs/Problem_2589.java
+++ src/baekjoon/gold/dfs_and_bfs/Problem_2589.java
@@ -1,12 +1,83 @@
 package baekjoon.gold.dfs_and_bfs;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+/*
+theme: 육지로 갈 수 있는 곳 중에서 가장 멀리 떨어진 땅 간의 최단 거리를 구하자.
+
+- dfs 를 통해 파고들면서 도달할 수 있는(육지이고, 아직 방문하지 않은) 곳 중에서 가장 멀리 있는 곳으로 가야 한다.
+- 다만 가장 효율적으로 가야 하므로, a -> b 로 갈 수 있는 경우의 수들 중 가장 짧게 갈 수 있는 경우의 수를 찾아야 한다.
+    - 이를 위해, 각 경우의 수들 마다 해당 블럭까지의 걸린 거리를 기록한다. 매 블럭을 방문할 때마다 여태까지의 최단 거리보다 현재의 거리가 짧은지를 비교한다.
+    - 해당 과정을 통해 나온 최솟값이
+ */
 
 public class Problem_2589 {
-    public static void main(String[] args) {
+
+    static int[] dx = {0, 0, 1, -1};
+    static int[] dy=  {1, -1, 0, 0};
+    static char[][] map;
+    static boolean[][] isVisited;
+    static List<Integer> maxDistances;
+    static int distance;
+    static int answer;
+    static int h;
+    static int w;
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        h = Integer.parseInt(st.nextToken());
+        w = Integer.parseInt(st.nextToken());
+        map = new char[h][w];
+        isVisited = new boolean[h][w];
+        maxDistances = new ArrayList<>();
+
+        // init
+        for (int i = 0; i < h; i++) {
+            String s = br.readLine();
+            for (int j = 0; j < w; j++) {
+                map[i][j] = s.charAt(j);
+            }
+        }
+
+        // logic
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j < w; j++) {
+                if (map[i][j] == 'L') {
+                    answer = 0;
+                    dfs(i, j, 0);
+                    maxDistances.add(answer);
+                }
+            }
+        }
+
+        System.out.println(answer);
+    }
+
+    private static void dfs(int i, int j, int distance) {
+        if (!isVisited[i][j] || map[i][j] == 'W') { // 이미 방문했거나 방문하려는 곳이 바다일 때
+            answer = Math.max(answer, distance);
+            return;
+        }
+        isVisited[i][j] = true;
+
+        for (int k = 0; k < 4; k++) {
+            int cx = i + dx[k];
+            int cy = j + dy[k];
+
+            if (cx >= 0 && cy >= 0 && cx < h && cy < w) {
+                if (!isVisited[cx][cy] && map[cx][cy] == 'L') {
+                    isVisited[cx][cy] = true;
+                    dfs(cx, cy, distance + 1);
+                }
+            }
+        }
 
     }
 }
