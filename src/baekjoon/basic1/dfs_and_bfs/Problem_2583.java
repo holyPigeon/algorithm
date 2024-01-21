65dc3d37bae5440d251f3affcf0304c297bd11a0
jae woo Jung <awfjol2008@naver.com>
2024-01-21 22:35:08 +0900

    2583 문제 부분 풀이 시작

---
+++ src/baekjoon/basic1/dfs_and_bfs/Problem_2583.java
@@ -0,0 +1,13 @@
+package baekjoon.basic1.dfs_and_bfs;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Problem_2583 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        br.readLine();
+    }
+}
