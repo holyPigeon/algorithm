25bfa2937118416f65f089a0213ee6a084066b88
jae woo Jung <awfjol2008@naver.com>
2024-01-23 17:03:56 +0900

    9079 문제 포기

--- src/baekjoon/basic1/dfs_and_bfs/Problem_9079.java
+++ src/baekjoon/basic1/dfs_and_bfs/Problem_9079.java
@@ -1,10 +1,29 @@
 package baekjoon.basic1.dfs_and_bfs;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Problem_9079 {
-    public static void main(String[] args) {
+
+    static char[][] map;
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int testcase = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < testcase; i++) {
+            // init
+            for (int j = 0; j < 3; j++) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                for (int k = 0; k < 3; k++) {
+                    map[j][k] = st.nextToken().charAt(0);
+                }
+            }
+            // logic
+
+        }
     }
 }
