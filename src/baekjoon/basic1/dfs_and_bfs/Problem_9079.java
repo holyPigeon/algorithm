a405ea9676b13612a0947956e292cc53588bfbff
jae woo Jung <awfjol2008@naver.com>
2024-01-23 16:47:58 +0900

    9079 문제 부분 풀이 시작

---
+++ src/baekjoon/basic1/dfs_and_bfs/Problem_9079.java
@@ -0,0 +1,10 @@
+package baekjoon.basic1.dfs_and_bfs;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Problem_9079 {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    }
+}
