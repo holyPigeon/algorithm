4a6fbc6b43bb61cc3c23e42a6410fdb28d8445be
jae woo Jung <awfjol2008@naver.com>
2024-02-03 13:15:56 +0900

    2665 문제 풀이 시작

---
+++ src/baekjoon/gold/dfs_and_bfs/Problem_2665.java
@@ -0,0 +1,10 @@
+package baekjoon.gold.dfs_and_bfs;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Problem_2665 {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    }
+}
