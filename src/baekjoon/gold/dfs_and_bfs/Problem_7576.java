1fa8e05ab180491e36f317790279bdfa4a275f70
jae woo Jung <awfjol2008@naver.com>
2024-02-03 13:12:53 +0900

    7576 문제 풀이 완료

--- src/baekjoon/gold/dfs_and_bfs/Problem_7576.java
+++ src/baekjoon/gold/dfs_and_bfs/Problem_7576.java
@@ -25,5 +25,58 @@ public class Problem_7576 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        m = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+        map = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st2 = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                int tomato = Integer.parseInt(st2.nextToken());
+                map[i][j] = tomato;
+                if (tomato == 1) {
+                    queue.offer(new int[]{i, j});
+                }
+            }
+        }
+
+        bfs();
+
+        int day = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                day = Math.max(day, map[i][j]);
+                if (map[i][j] == 0) {
+                    System.out.println(-1);
+                    return;
+                }
+            }
+        }
+
+        System.out.println(day - 1);
+    }
+
+    private static void bfs() {
+        while(!queue.isEmpty()) {
+            int[] poll = queue.poll();
+            int y = poll[0];
+            int x = poll[1];
+
+            for (int i = 0; i < 4; i++) {
+                int cy = y + dy[i];
+                int cx = x + dx[i];
+
+                if (isAvailablePoint(cy, cx)) {
+                    map[cy][cx] = map[y][x] + 1;
+                    queue.offer(new int[]{cy, cx});
+                }
+            }
+        }
+    }
+
+    private static boolean isAvailablePoint(int cy, int cx) {
+        return (cy >= 0 && cx >= 0 && cy < n && cx < m)  && (map[cy][cx] == 0);
     }
 }
