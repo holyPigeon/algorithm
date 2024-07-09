783729ec6ee2a729c5d0f2731795f6e793f46b25
jae woo Jung <awfjol2008@gmail.com>
2024-07-09 19:30:07 +0900

    1260 풀이 완료

--- src/baekjoon/gold/dfs_and_bfs/Problem_1504.java
+++ src/baekjoon/gold/dfs_and_bfs/Problem_1504.java
@@ -1,6 +1,7 @@
 package baekjoon.gold.dfs_and_bfs;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.lang.reflect.Array;
 import java.util.*;
@@ -23,7 +24,7 @@ public class Problem_1504 {
     static int e;
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
