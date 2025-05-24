ffa6081f3d57503ace50ef3d4e56f201323c9ec5
jae woo Jung <awfjol2008@gmail.com>
2025-05-24 22:08:37 +0900

    solve problem 12605

---
+++ src/baekjoon/solutio/study_05_23/practice/Problem_12605_Stack_B5.java
@@ -0,0 +1,41 @@
+package baekjoon.solutio.study_05_23.practice;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+/**
+ * 단어들을 반대 순서로 뒤집어라 -> 나중에 들어온 애들이 먼저 나온다 -> LIFO
+ *
+ */
+
+public class Problem_12605_Stack_B5 {
+    public static void main(String[] args) throws IOException {
+
+        // read
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            Stack<String> stack = new Stack<>();
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            while(st.hasMoreTokens()) {
+                stack.push(st.nextToken());
+                System.out.println(st.countTokens());
+            }
+
+            System.out.print("Case #" + (i + 1) + ": ");
+            while (!stack.isEmpty()) {
+                System.out.print(stack.pop() + " ");
+            }
+            System.out.println();
+        }
+    }
+}
