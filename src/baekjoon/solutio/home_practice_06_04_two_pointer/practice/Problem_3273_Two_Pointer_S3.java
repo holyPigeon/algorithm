ee2d3b6ca4c7ffe231837999c96f05cb15c53473
jae woo Jung <awfjol2008@gmail.com>
2025-06-06 21:50:54 +0900

    백준 3273 문제 풀이 완료

---
+++ src/baekjoon/solutio/home_practice_06_04_two_pointer/practice/Problem_3273_Two_Pointer_S3.java
@@ -0,0 +1,51 @@
+package baekjoon.solutio.home_practice_06_04_two_pointer.practice;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Problem_3273_Two_Pointer_S3 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int X = Integer.parseInt(br.readLine());
+
+        List<Integer> numbers = new ArrayList<>();
+        while(st.hasMoreTokens()) {
+            int number = Integer.parseInt(st.nextToken());
+            numbers.add(number);
+        }
+
+        Collections.sort(numbers);
+
+        int left = 0;
+        int right = numbers.size() - 1;
+        int count = 0;
+
+        while (left < right) {
+            int sum = numbers.get(left) + numbers.get(right);
+
+            if (sum == X) {
+                left++;
+                right--;
+                count++;
+            }
+            if (sum < X) {
+                left++;
+            }
+            if (sum > X) {
+                right--;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
+
+// 1 2 3 5 7 9 10 11 12
