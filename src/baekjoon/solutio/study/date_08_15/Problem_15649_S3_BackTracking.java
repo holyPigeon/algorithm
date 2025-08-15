b4e87b6642778cc0e5064429fd64df48957d7fed
jae woo Jung <awfjol2008@gmail.com>
2025-08-15 15:29:40 +0900

    백준 15649 재풀이 완료

---
+++ src/baekjoon/solutio/study/date_08_15/Problem_15649_S3_BackTracking.java
@@ -0,0 +1,42 @@
+package baekjoon.solutio.study.date_08_15;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Problem_15649_S3_BackTracking {
+    static int[] arr;
+    static boolean[] visited;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        arr = new int[M];
+        visited = new boolean[N];
+
+        dfs(N, M, 0);
+    }
+
+    private static void dfs(int N, int M, int depth) {
+        if (depth == M) {
+            for (int i : arr) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                arr[depth] = i + 1;
+                dfs(N, M, depth + 1);
+                visited[i] = false;
+            }
+        }
+    }
+}
